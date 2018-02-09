f3ddf79d343ebfe6b3021b018ef2c68ab4abc80d
Vyacheslav GLADUSH <vgladush@e1r11p4.unit.ua>
2018-02-09 19:15:17 +0200

    correct

--- ft_dblhex.c
+++ ft_dblhex.c
@@ -6,7 +6,7 @@
 /*   By: vgladush <vgladush@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 13:50:22 by vgladush          #+#    #+#             */
-/*   Updated: 2018/01/15 16:13:29 by vgladush         ###   ########.fr       */
+/*   Updated: 2018/01/15 16:22:39 by vgladush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,16 +36,16 @@ static	void	hexsec(uintmax_t z, char **s, int *i, int y)
 {
 	int			j;
 
+	j = 12;
 	if (i[11] && z != 0)
 		s[1] = ft_itoabase(z, 16, y);
 	else
 		s[1] = ft_strdup("");
-	if (j == i[11] || (i[11] < 0 && j == 13))
-		while (s[1][--j] == '0')
-			s[1][j] = '\0';
 	while ((int)ft_strlen(s[1]) < i[11])
 		s[1] = ft_joinfree("0", s[1], 2);
-	j = (int)ft_strlen(s[1]);
+	if (ft_strlen(s[1]) == 13)
+		while (s[1][j] == '0')
+			s[1][j--] = '\0';
 	s[0] = ft_itoabase(i[16], 1, 0);
 	if (s[0][0] != '-')
 		s[0] = ft_joinfree("+", s[0], 2);
